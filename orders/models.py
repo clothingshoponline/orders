998f120ee3966c8061fc3db486f74b741152d08f
Nazneen Peracha <56941470+nazneenperacha@users.noreply.github.com>
2020-09-15 11:33:53 -0700

    Change ''' to """ in docstrings

--- orders/models.py
+++ orders/models.py
@@ -10,7 +10,7 @@ class Line:
         self.qty = qty
 
     def __str__(self) -> str:
-        ''' Define the string representation of a Line. '''
+        """Define the string representation of a Line."""
         return f'{self.sku}: {self.qty}'
 
 
@@ -24,7 +24,7 @@ class Package:
         self._lines = dict()
 
     def __str__(self) -> str:
-        ''' Return the string representation of a Package. '''
+        """Return the string representation of a Package."""
         package_str = self.invoice
         skus = sorted(list(self._lines.keys()))
         for sku in skus:
@@ -32,19 +32,20 @@ class Package:
         return package_str
     
     def add(self, sku: str, qty: int) -> None:
-        ''' If sku already exists in self._lines, add qty to 
-        self._lines[sku]. Otherwise, add Line(sku, qty) to self._lines. '''
+        """If sku already exists in self._lines, add qty to 
+        self._lines[sku]. Otherwise, add Line(sku, qty) to self._lines.
+        """
         if sku in self._lines:
             self._lines[sku].qty += qty
         else:
             self._lines[sku] = Line(sku, qty)
 
     def lines(self) -> [Line]:
-        ''' Return lines stored in self._lines as a list of Lines. '''
+        """Return lines stored in self._lines as a list of Lines."""
         return [line for line in self._lines.values()]
 
     def contains(self, sku: str, qty: int) -> bool:
-        ''' Return True if the Package contains the qty of sku, False otherwise. '''
+        """Return True if the Package contains the qty of sku, False otherwise."""
         return sku in self._lines and self._lines[sku].qty >= qty
 
 
@@ -68,9 +69,10 @@ class Order:
         return order_str
 
     def add(self, sku: str, qty: int, invoice: str) -> None:
-        ''' Add Line(sku, qty) to self._packages[invoice]. If the sku 
+        """Add Line(sku, qty) to self._packages[invoice]. If the sku 
         does not exist, create a new Line. If the invoice does not exist,
-        create a new package. '''
+        create a new package.
+        """
         if invoice in self._packages:
             self._packages[invoice].add(sku, qty)
         else:
@@ -79,21 +81,23 @@ class Order:
             self._packages[invoice] = package
 
     def lines_in_package(self, invoice: str) -> [Line]:
-        ''' Returns a list of Lines within the Package with 
+        """Returns a list of Lines within the Package with 
         the given invoice. Raise a ValueError if the order 
-        does not contain the invoice. '''
+        does not contain the invoice.
+        """
         if invoice not in self._packages:
             raise ValueError('invoice does not exist')
         return self._packages[invoice].lines()
 
     def invoice_containing(self, sku: str, qty: int) -> str:
-        ''' Return the invoice of the package containing the given sku. 
-        Raise a ValueError if the given sku is not within the order. '''
+        """Return the invoice of the package containing the given sku. 
+        Raise a ValueError if the given sku is not within the order.
+        """
         for package in self._packages.values():
             if package.contains(sku, qty):
                 return package.invoice
         raise ValueError('qty of sku does not exist')
 
     def invoices(self) -> [str]:
-        ''' Return a list of invoices within the order. '''
+        """Return a list of invoices within the order."""
         return sorted(list(self._packages.keys()))
